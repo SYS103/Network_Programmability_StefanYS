2ae8fbb0306a7b3ad247deb0ed1aefc40360e2ca
SYS103 <stefan.sutanto@outlook.com>
2025-01-02 16:07:44 +0000

    Opdracht Netmiko

--- PythonExperiments/netmiko/netmiko_create_loopback_int.py
+++ PythonExperiments/netmiko/netmiko_create_loopback_int.py
@@ -3,11 +3,10 @@ from netmiko import ConnectHandler
 IP_ADDRESS = "192.168.56.101" #"Get IP Address"
 sshCli = ConnectHandler(
     device_type="cisco_ios",
-    host=IP_ADDRESS,
+    host="10.10.20.48",
     port="22",
-    
-    username="cisco",
-    password="cisco123!"
+    username="developer",
+    password="C1sco12345"
     )
 config_commands = (
     'interface loopback 101' , 'ip address 10.1.0.101 255.255.255.0'
--- PythonExperiments/netmiko/netmiko_ssh_show_interface.py
+++ PythonExperiments/netmiko/netmiko_ssh_show_interface.py
@@ -2,10 +2,10 @@ print("Connecting via SSH => show interface status (brief)")
 from netmiko import ConnectHandler
 sshCli = ConnectHandler(
     device_type="cisco_ios",
-    host="192.168.56.101",
+    host="10.10.20.48",
     port="22",
-    username="cisco",
-    password="cisco123!"
+    username="developer",
+    password="C1sco12345"
     )
 output=sshCli.send_command("show ip interface brief")
 print(output)
